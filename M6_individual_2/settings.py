0f5442f032163d044ccef316dbc4c8d934c6f4cd
jvielmac <jacco5v@gmail.com>
2023-07-22 17:06:43 -0400

    Terminado landing page

--- M6_individual_2/settings.py
+++ M6_individual_2/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mi_app.apps.MiAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'M6_individual_2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['mi_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mi_app/urls.py
+++ mi_app/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('mi_app.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.mainpage, name='main-page'),
 ]
